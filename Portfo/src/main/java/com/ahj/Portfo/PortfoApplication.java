2c20795039c158818621c0ca945e26c82dd50d4b
wow4423 <wow4423@gmail.com>
2026-06-15 15:16:35 +0900

    Initial commit

---
+++ Portfo/src/main/java/com/ahj/Portfo/PortfoApplication.java
@@ -0,0 +1,13 @@
+package com.ahj.Portfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfoApplication.class, args);
+	}
+
+}
---
+++ Portfo/src/test/java/com/ahj/Portfo/PortfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahj.Portfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
